1a98530f7a65a76f1dd7faaa188951a3efe43847
Wagner Parisoto <wagnerparisoto@gmail.com>
2018-08-25 21:05:09 -0300

    ajustes processo de build da aplicacao para producai

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
-import { HttpModule } from '@angular/http';
-import { RouterModule, PreloadAllModules } from '@angular/router';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, LOCALE_ID } from "@angular/core";
+import { HttpModule } from "@angular/http";
+import { RouterModule, PreloadAllModules } from "@angular/router";
 
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { HomeComponent } from "./home/home.component";
 
-import { ROUTES } from './app.route';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
-import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import { MenuComponent } from './restaurant-detail/menu/menu.component';
-import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
-import { ReviewsComponent } from './restaurant-details/reviews/reviews.component';
+import { ROUTES } from "./app.route";
+import { RestaurantsComponent } from "./restaurants/restaurants.component";
+import { RestaurantComponent } from "./restaurants/restaurant/restaurant.component";
+import { RestaurantDetailComponent } from "./restaurant-detail/restaurant-detail.component";
+import { MenuComponent } from "./restaurant-detail/menu/menu.component";
+import { ShoppingCartComponent } from "./restaurant-detail/shopping-cart/shopping-cart.component";
+import { MenuItemComponent } from "./restaurant-detail/menu-item/menu-item.component";
+import { ReviewsComponent } from "./restaurant-details/reviews/reviews.component";
 
-import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
-import { SharedModule } from './shared/shared.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NotFoundComponent } from './not-found/not-found.component';
-
-
+import { OrderSumaryComponent } from "./order-sumary/order-sumary.component";
+import { SharedModule } from "./shared/shared.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { NotFoundComponent } from "./not-found/not-found.component";
+import { LocationStrategy, HashLocationStrategy } from "@angular/common";
 
 @NgModule({
   declarations: [
@@ -43,9 +42,12 @@ import { NotFoundComponent } from './not-found/not-found.component';
     BrowserAnimationsModule,
     HttpModule,
     SharedModule.forRoot(),
-    RouterModule.forRoot(ROUTES, {preloadingStrategy: PreloadAllModules})
+    RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules })
   ],
-  providers: [{provide: LOCALE_ID, useValue: 'pt-BR'}],
+  providers: [
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
+    { provide: LOCALE_ID, useValue: "pt-BR" }
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
